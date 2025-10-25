740e51dd651f3721bec19fb3f985943fc6620b92
Plazum753 <antoine.garcia753@gmail.com>
2025-10-25 11:48:21 +0200

    ajout de TROIs et de livres et du random

--- programme.py
+++ programme.py
@@ -1,5 +1,5 @@
 from collections import Counter
-from random import choice
+from random import choice, choices
 
 with open("texte.txt",'r', encoding='utf-8') as f:
     texte = f.read()
@@ -12,13 +12,16 @@ for i in range(len(texte)-1):
     dico[(texte[i],texte[i+1])] += 1
 
 def Un(phrase):
-    mot = choice(list(dico.keys()))[0]
-    maxi = 0
+    mots = []
+    poids = []
     for e in dico :
-        if phrase[-1] == e[0] and maxi < dico[e]:
-            mot = e[1]
-            maxi = dico[e]
-    return mot
+        if phrase[-1] == e[0] :
+            mots.append(e[1])
+            poids.append(dico[e])
+    if mots != []:
+        return choices(mots,poids)[0]
+    else :
+        return choice(list(dico.keys()))[0]
 
 
 dico2 = Counter()
@@ -28,21 +31,49 @@ for i in range(len(texte)-2):
 
 
 def DEUx(phrase):
-    mot = None
-    maxi = 0
+    if len(phrase)<2:
+        return None
+    mots = []
+    poids = []
     for e in dico2:
-        if phrase[-2] == e[0] and phrase[-1] == e[1] and maxi < dico2[e]:
-            mot = e[2]
-            maxi = dico2[e]
-    return mot
+        if phrase[-2] == e[0] and phrase[-1] == e[1] :
+            mots.append(e[2])
+            poids.append(dico2[e])
+    if mots != []:
+        return choices(mots,poids)[0]
+    else :
+        return None
+    
+dico3 = Counter()
+
+for i in range(len(texte)-3):
+    dico3[(texte[i],texte[i+1],texte[i+2],texte[i+3])] += 1
+
+
+def TROIs(phrase):
+    if len(phrase)<3:
+        return None
+    mots = []
+    poids = []
+    for e in dico3:
+        if phrase[-3] == e[0] and phrase[-2] == e[1] and phrase[-1] == e[2] :
+            mots.append(e[3])
+            poids.append(dico3[e])
+    if mots != []:
+        return choices(mots,poids)[0]
+    else :
+        return None
 
 def generateur(phrase):
-    mot = DEUx(phrase)
+    mot = TROIs(phrase)
+    if mot == None:
+        mot = DEUx(phrase)
     if mot == None:
         mot = Un(phrase)
     print(mot+" ", end="")
     phrase.append(mot)
     return phrase
+
 
 
 phrase = input("Parle !\n")
